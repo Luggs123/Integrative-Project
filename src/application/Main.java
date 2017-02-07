3f4e4b427b0da3d69e75e63a0f2df5fc0a1c94e4
Luggs123 <sunsplashin@hotmail.com>
2017-02-07 11:54:39 -0500

    For Mark, with love

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,14 +4,19 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root = new VBox();
 			Scene scene = new Scene(root,400,400);
+			Rectangle rect = new Rectangle(100, 100, 100, 100);
+			root.getChildren().add(rect);
+			root.getChildren().remove(rect);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
