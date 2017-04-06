fc033a81944d8d93a033dff7b55e820044e1d261
Luggs123 <sunsplashin@hotmail.com>
2017-04-06 13:30:16 -0400

    Copied base for ClsEle

---
+++ src/pkg_EM/ClsEle.java
@@ -0,0 +1,51 @@
+package pkg_EM;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import pkg_main.AppButton;
+import pkg_main.AppTextField;
+
+public class ClsEle implements pkg_main.IConstants {
+	
+	// Buttons
+	public static AppButton btnStart;
+	public static AppButton btnDone;
+	public static AppButton btnPause;
+	public static AppButton btnReset;
+	public static AppButton btnHelp;
+	
+	// Text Fields
+	private static AppTextField txtGrav;
+	private static AppTextField txtAng;
+	private static AppTextField txtVel;
+	
+	// Charts
+	private static LineChart<DoubleProperty, DoubleProperty> chrtVel;
+	private static LineChart<DoubleProperty, DoubleProperty> chrtAcc;
+	
+	public static Pane drawScene() {
+		
+		// Main VBox.
+		VBox winProj = new VBox();
+		
+		// Sub Windows
+		Pane winDisplay = new Pane();
+		Pane winButt = new Pane();
+		Pane winInfo = new Pane();
+		Pane winHelp = new Pane();
+		Label cringeuh = new Label("idkf");
+		
+		// Add all the panes to the main window.
+		HBox separator = new HBox(cringeuh);
+		separator.getChildren().addAll(winButt, winInfo);
+		
+		winProj.getChildren().addAll(winDisplay, separator, winHelp);
+		
+		return winProj;
+	}
+}
